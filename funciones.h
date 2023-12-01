0d366b22525fdc14449746ccbf596eb0637afb5c
carlos.guaita <carlos.guaita@udla.edu.ec>
2023-12-01 16:12:58 -0500

    se agrega funciones de perimetro y area 1

--- funciones.h
+++
@@ -1,4 +0,0 @@
-void leerPuntos(float puntos[][2], int filas, int columnas);
-void calcularDistancias(float puntos[][2],float distancias[]);
-void calcularPerimetro(float *perimetro, float distancias[]);
-void calcularArea(float *perimetro, float *area, float distancias[]);
