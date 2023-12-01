57e0b923a7b8d944b7a646cf9863d79e0da04356
carlos.guaita <carlos.guaita@udla.edu.ec>
2023-12-01 16:04:18 -0500

    Inicio

---
+++ funciones.h
@@ -0,0 +1,3 @@
+void leerPuntos(float puntos[][2], int filas, int columnas);
+void calcularDistancias(float puntos[][2],float distancias[]);
+void calcularPerimetroArea(float *perimetro, float *area, float distancias[]);
---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "funciones.h"
+
+int main (int argc, char *argv[]) {
+
+    float puntos[3][2], distancias[3];
+
+    float perimetro1, area2;
+
+    leerPuntos(puntos,3,2);
+    calcularDistancias(puntos,distancias);
+    calcularPerimetroArea(&perimetro1,&area2,distancias);
+
+    printf("El perimetro del triangulo es: %f\n",perimetro1);
+    printf("El area del triangulo es: %f\n",area2);
+
+    return 0;
+}
